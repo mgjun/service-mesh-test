83b0737269a22dc87e428d099cd736d37d90ad8f
emeng <eden.meng@activenetwork.com>
2019-12-02 00:23:00 +0800

    use java to test envoy

--- istio/envoy/demos/envoy-java/src/main/java/com/mgjun/envoyserver/TestController.java
+++ istio/envoy/demos/envoy-java/src/main/java/com/mgjun/envoyserver/TestController.java
@@ -1,32 +1,20 @@
 package com.mgjun.envoyserver;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/service")
 public class TestController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
-    
-    @GetMapping("/hello")
-    public String hello() {
-        LOGGER.info("get request from remote, send response, say hello");
-        return "hello";
-    }
-
-    @GetMapping("/hi")
-    public String hi() {
-        LOGGER.info("get request from remote, send response, say hi");
-        return "hi";
-    }
-
-    @GetMapping("/self")
-    public String self() {
-        LOGGER.info("get request from remote, send response, say self");
-        return "self";
+    @GetMapping("/{serviceName}")
+    public String get(@PathVariable String serviceName) throws UnknownHostException {
+        return String.format("get request from service{ %s }, local address: %s", serviceName, Inet4Address.getLocalHost()
+                                                                                                           .getHostAddress());
     }
 }
